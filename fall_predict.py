b601f6382756c076875e9ffec7276942ff0cb4fe
Adam Chambers <stiny3@gmail.com>
2016-05-11 13:00:13 -0400

    Added Many different classifiers (NB still working the best). Also rudimentary cross-validation.

--- fall_predict.py
+++ fall_predict.py
@@ -3,7 +3,7 @@ Classifiers to use:
 	GaussianNB
 	SVC
 	K-Nearest Neighbors
-	Decistion Tree/ Forest
+	Decision Tree/ Forest
 
 Features to use:
 	A_NormCad_Avg
@@ -25,7 +25,7 @@ df = pd.read_csv('Fixed_Flat_Surface.csv', sep=',')
 
 #populate features array
 build_features = []
-for i in range(len(df.SbjID)-1):
+for i in range(len(df.SbjID)):
 	build_features.append([
 		df.Age[i], 
 		df.A_NormCad_Avg[i], 
@@ -35,25 +35,62 @@ for i in range(len(df.SbjID)-1):
 		df.A_NormTstr_Avg[i]
 	])
 
-features_train = np.array(build_features)
+features_train = np.array(build_features[:40]+build_features[50:])
+features_test = np.array(build_features[40:50])
 
 #populate labels array
 build_labels = []
-for i in range(len(df.SbjID)-1):
-	build_labels.append([
+for i in range(len(df.SbjID)):
+	build_labels.append(
 		df.Falls[i]
-		])
+		)
 
-labels_train = np.array(build_labels)
+labels_train = np.array(build_labels[:40]+ build_labels[50:])
+labels_test = np.array(build_labels[40:50])
+
+'''
+print(features_train)
+print(labels_train)
+'''
+
 
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 clf = GaussianNB()
+#clf = SVC(kernel='rbf')
+#clf = KNeighborsClassifier()
+#clf = DecisionTreeClassifier()
+#clf = AdaBoostClassifier()
+#clf = RandomForestClassifier()
+
 clf.fit(features_train, labels_train)
+
+pred = []
+for i in features_test:
+	pred.append(clf.predict(i))
+
+
+predictions = np.array(pred)
+'''
 print(clf.predict([
-	df.Age[4], 
-	df.A_NormCad_Avg[4], 
-	df.A_SWPperc_Avg[4], 
-	df.A_SWPperc_CV[4], 
-	df.A_SpeedNorm_Avg[4],
-	df.A_NormTstr_Avg[4]
+	df.Age[74], 
+	df.A_NormCad_Avg[74], 
+	df.A_SWPperc_Avg[74], 
+	df.A_SWPperc_CV[74], 
+	df.A_SpeedNorm_Avg[74],
+	df.A_NormTstr_Avg[74]
 ]))
+'''
+from sklearn.metrics import accuracy_score
+
+print predictions
+print labels_test
+
+print(accuracy_score(labels_test,predictions))
+
+
