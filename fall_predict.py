b0212412d6c666df21aee97872655c9d2179eb39
Adam Chambers <stiny3@gmail.com>
2016-05-10 20:13:28 -0400

    Playing around with a few different classifiers for predicting falls in the elderly. Starting with GaussianNB

---
+++ fall_predict.py
@@ -0,0 +1,47 @@
+"""
+Classifiers to use:
+	GaussianNB
+	SVC
+	K-Nearest Neighbors
+	Decistion Tree/ Forest
+
+Features to use:
+	A_NormCad_Avg
+	A_SWPperc_Avg
+	A_NormTstr_Avg (Stride Time)
+	A_SWPperc_CV
+	A_SpeedNorm_Avg
+	Age
+
+"""
+
+
+
+import numpy as np
+import pandas as pd
+
+#create dataframe from csv file
+df = pd.read_csv('Fixed_Flat_Surface.csv', sep=',')
+
+#populate features array
+build_features = []
+for i in range(len(df.SbjID)-1):
+	build.append([df.Age[i], 
+		df.A_NormCad_Avg[i], 
+		df.A_SWPperc_Avg[i], 
+		df.A_SWPperc_CV[i], 
+		df.A_SpeedNorm_Avg[i],
+		df.A_NormTstr_Avg[i])
+
+features_train = np.array(build_features)
+
+#populate labels array
+build_labels = []
+for i in df.
+
+
+labels_train = np.array(build_labels)
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print(clf.predict([]))
