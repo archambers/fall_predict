9d2e00708636a9e04437c8db56e1d2c425ec8d79
Adam Chambers <stiny3@gmail.com>
2016-05-11 17:03:29 -0400

    Added Graph function

--- fall_predict.py
+++ fall_predict.py
@@ -1,9 +1,4 @@
 """
-Classifiers to use:
-	GaussianNB
-	SVC
-	K-Nearest Neighbors
-	Decision Tree/ Forest
 
 Features to use:
 	A_NormCad_Avg
@@ -28,15 +23,21 @@ build_features = []
 for i in range(len(df.SbjID)):
 	build_features.append([
 		df.Age[i], 
-		df.A_NormCad_Avg[i], 
-		df.A_SWPperc_Avg[i], 
-		df.A_SWPperc_CV[i], 
-		df.A_SpeedNorm_Avg[i], 
+		#df.A_NormCad_Avg[i], 
+		#df.A_SWPperc_Avg[i], 
+		#df.A_SWPperc_CV[i], 
+		#df.A_SpeedNorm_Avg[i], 
 		df.A_NormTstr_Avg[i]
 	])
 
 features_train = np.array(build_features[:40]+build_features[50:])
+features_train[:,0] /= features_train[:,0].max()
+features_train[:,1] /= features_train[:,1].max()
+
 features_test = np.array(build_features[40:50])
+features_test[:,0] /= features_test[:,0].max()
+features_test[:,1] /= features_test[:,1].max()
+
 
 #populate labels array
 build_labels = []
@@ -86,11 +87,54 @@ print(clf.predict([
 	df.A_NormTstr_Avg[74]
 ]))
 '''
+
+
 from sklearn.metrics import accuracy_score
 
-print predictions
-print labels_test
+print str(np.ravel(predictions)) + " : predictions"
+print str(labels_test) + " : true"
 
 print(accuracy_score(labels_test,predictions))
 
+
+#print features_train[:,0]
+
+
+import matplotlib.pyplot as plt
+import pylab as pl
+
+x_min = 0.0; x_max = 1.0
+y_min = 0.0; y_max = 1.0
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, m_max]x[y_min, y_max].
+h = .01  # step size in the mesh
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+
+plt.pcolormesh(xx, yy, Z, cmap=pl.cm.seismic)
+
+
+
+
+
+from pylab import show
+
+vAge = features_train[:,0]
+vSpeed = features_train[:,1]
+
+vAge2 = features_test[:,0]
+vSpeed2 = features_test[:,1]
+
+plt.scatter(vAge2, vSpeed2, c=labels_test)
+plt.scatter(vAge, vSpeed, c=labels_train)
+
+
+
+show()
 
