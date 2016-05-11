7a2414480de25c0a296748fc8c8a94b00e574115
Adam Chambers <stiny3@gmail.com>
2016-05-10 20:34:37 -0400

    update

--- fall_predict.py
+++ fall_predict.py
@@ -26,22 +26,34 @@ df = pd.read_csv('Fixed_Flat_Surface.csv', sep=',')
 #populate features array
 build_features = []
 for i in range(len(df.SbjID)-1):
-	build.append([df.Age[i], 
+	build_features.append([
+		df.Age[i], 
 		df.A_NormCad_Avg[i], 
 		df.A_SWPperc_Avg[i], 
 		df.A_SWPperc_CV[i], 
-		df.A_SpeedNorm_Avg[i],
-		df.A_NormTstr_Avg[i])
+		df.A_SpeedNorm_Avg[i], 
+		df.A_NormTstr_Avg[i]
+	])
 
 features_train = np.array(build_features)
 
 #populate labels array
 build_labels = []
-for i in df.
-
+for i in range(len(df.SbjID)-1):
+	build_labels.append([
+		df.Falls[i]
+		])
 
 labels_train = np.array(build_labels)
+
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 clf.fit(features_train, labels_train)
-print(clf.predict([]))
+print(clf.predict([
+	df.Age[4], 
+	df.A_NormCad_Avg[4], 
+	df.A_SWPperc_Avg[4], 
+	df.A_SWPperc_CV[4], 
+	df.A_SpeedNorm_Avg[4],
+	df.A_NormTstr_Avg[4]
+]))
